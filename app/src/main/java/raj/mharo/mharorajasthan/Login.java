48a358820d1dc68cb36c299cb2d2129b20eb7b12
karan1k03 <karankharbanda.78@gmail.com>
2017-03-20 13:38:00 +0530

    Commit3

---
+++ app/src/main/java/raj/mharo/mharorajasthan/Login.java
@@ -0,0 +1,34 @@
+package raj.mharo.mharorajasthan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    Button Login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        email= (EditText) findViewById(R.id.input_email);
+        password= (EditText) findViewById(R.id.input_password);
+        Login= (Button) findViewById(R.id.btn_login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        welcome.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
