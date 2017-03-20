1e80a0b25ebee95e899d5331177e09340a0da6c5
karan1k03 <karankharbanda.78@gmail.com>
2017-03-21 01:04:25 +0530

    commit11

--- app/src/main/java/raj/mharo/mharorajasthan/Toll.java
+++ app/src/main/java/raj/mharo/mharorajasthan/Toll.java
@@ -1,8 +1,11 @@
 package raj.mharo.mharorajasthan;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -29,10 +32,35 @@ public class Toll extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(Toll.this)
+                                .setSmallIcon(R.drawable.toll)
+                                .setContentTitle("Toll booth ahead! 2 kms left")
+                                .setContentText("Click to pay");
+
+                Intent resultIntent = new Intent(Toll.this, Payment.class);
+
+// Because clicking the notification opens a new ("special") activity, there's
+// no need to create an artificial back stack.
+                PendingIntent resultPendingIntent =
+                        PendingIntent.getActivity(
+                                Toll.this,
+                                0,
+                                resultIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                mBuilder.setContentIntent(resultPendingIntent);
+
+//ts an ID for the notification
+                int mNotificationId = 001;
+// Gets an instance of the NotificationManager service
+                NotificationManager mNotifyMgr =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+                mNotifyMgr.notify(mNotificationId, mBuilder.build());
             }
         });
+
     }
 
 
