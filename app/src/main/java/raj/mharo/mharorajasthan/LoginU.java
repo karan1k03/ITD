2914df7935ee460b359e6e8715568b71fd1b8c31
prakash57278 <pRAKASHRAWAT2@GMAIL.COM>
2017-03-21 04:21:41 +0530

    Merge remote

--- app/src/main/java/raj/mharo/mharorajasthan/LoginU.java
+++ app/src/main/java/raj/mharo/mharorajasthan/LoginU.java
@@ -12,12 +12,25 @@ import android.widget.EditText;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class LoginU extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class LoginU extends AppCompatActivity {
+    JSONObject jsonObject;
     EditText email;
     EditText password;
     Button Login;
     String pass,username;
+    HttpURLConnection connection;
+    BufferedReader reader=null;
+    static JSONObject jObj = null;
+    static String json = "";
+    StringBuffer buffer=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +43,13 @@ public class LoginU extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new LoginRequest().execute();
+                new LoginRequest1().execute();
+                username=email.getText().toString();
+                pass=password.getText().toString();
             }
         });
     }
-    public class LoginRequest extends AsyncTask<String,String,String>
+    public class LoginRequest1 extends AsyncTask<String,String,String>
     {
         private ProgressDialog pDialog;
         protected void onPreExecute() {
@@ -50,34 +65,57 @@ public class LoginU extends AppCompatActivity {
         @Override
         protected String doInBackground(String... strings)
         {
-            username=email.getTex().toString();
-            pass=password.getText().toString();
-            UserFunctions userFunction = new UserFunctions();
-            JSONObject json = null;
+
             try {
-                json = userFunction.loginUser(username,"Test1234_");
+                URL loginUrl=new URL("http://www.itsohkay.in/mharo_rajasthan_api/merchant_toll.php");
+                connection=(HttpURLConnection)loginUrl.openConnection();
+                connection.setRequestMethod("POST");
+                connection.connect();
+                jsonObject=new JSONObject();
+                jsonObject.put("ssoid",email);
+                jsonObject.put("pass",pass);
+                OutputStreamWriter details=new OutputStreamWriter(connection.getOutputStream());
+                details.write(jsonObject.toString());
+                details.flush();
+
+                reader=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                buffer=new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                json=buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
+            } finally {
+                if(connection!=null)
+                {
+                    connection.disconnect();
+                }
             }
-            JSONObject parentObject= null;
             try {
-                parentObject = new JSONObject(json.toString());
+                jObj = new JSONObject(json);
             } catch (JSONException e) {
-                e.printStackTrace();
+
             }
-            try {
+            /*try {
 
                 int Status=parentObject.getInt("Status");
                 String ErrorMessage=parentObject.getString("ErrorMessage");
                 String ExtendedErrorMessage=parentObject.getString("ExtendedErrorMessage");
                 if(Status==200&&(ErrorMessage!=null&&ErrorMessage.isEmpty())&&(ExtendedErrorMessage!=null&&ExtendedErrorMessage.isEmpty()))
                 {
-                    session.createLoginSession(username,password);
+                   // session.createLoginSession(username,password);
                     return json.toString();
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
-            }
+            }*/
             return null;
         }
 
@@ -86,7 +124,7 @@ public class LoginU extends AppCompatActivity {
             super.onPostExecute(s);
             pDialog.setMessage("Loading User Space");
             pDialog.setTitle("Getting Data");
-            Intent intent = new Intent(getApplicationContext(), AfterLogin.class);
+            Intent intent = new Intent(getApplicationContext(), welcomeU.class);
             intent.putExtra("JsonResponse", s);
             startActivity(intent);
 
